4de7808f8678c61967964eada2431088f568f08f
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-07 23:37:48 +0100

    created database of type - array

---
+++ src/main/java/com/aprilAssessment/HRMS/Database/Database.java
@@ -0,0 +1,10 @@
+package com.aprilAssessment.HRMS.Database;
+
+import com.aprilAssessment.HRMS.entity.Property;
+
+public class Database {
+
+    protected static Property[] properties = new Property[5];
+    protected static int counter = 0;
+
+}
